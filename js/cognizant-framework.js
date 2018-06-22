2bd51ba006a41ded47818fd245213b8329aff0bc
Daniel Lewandowski <dlewandowski38@yahoo.com>
2018-06-22 17:53:34 -0400

    Add Succinct plugin controls

--- js/cognizant-framework.js
+++ js/cognizant-framework.js
@@ -64,6 +64,7 @@
 // @codekit-prepend '../bower_components/scrollup/dist/jquery.scrollUp.js'
 // @codekit-prepend '../bower_components/lazysizes/lazysizes.js'
 // @codekit-prepend '../bower_components/slick-carousel/slick/slick.min.js'
+// @codekit-prepend '../bower_components/succinct/jQuery.succinct.js'
 
 Foundation.Abide.defaults.patterns['dashes_only'] = /^[0-9-+]*$/;
 
@@ -331,3 +332,51 @@ $(document).ready(function() {
 
 });
 // -----  End of SLICK CAROUSEL  -----------------------------------------------------------------------
+
+
+
+// =============================================================================================
+// SUCCINCT - LINE CLAMP/TRUNCATE
+// =============================================================================================
+
+$(function() {
+    $('.truncate-40').succinct({
+        size: 40
+    });
+    $('.truncate-50').succinct({
+        size: 50
+    });
+    $('.truncate-60').succinct({
+        size: 60
+    });
+    $('.truncate-70').succinct({
+        size: 70
+    });
+    $('.truncate-75').succinct({
+        size: 75
+    });
+    $('.truncate-80').succinct({
+        size: 80
+    });
+    $('.truncate-100').succinct({
+        size: 100
+    });
+    $('.truncate-110').succinct({
+        size: 110
+    });
+    $('.truncate-120').succinct({
+        size: 120
+    });
+    $('.truncate-130').succinct({
+        size: 130
+    });
+    $('.truncate-140').succinct({
+        size: 140
+    });
+    $('.truncate-150').succinct({
+        size: 150
+    });
+});
+
+
+// -----  End of SUCCINCT - LINE CLAMP/TRUNCATE  ---------------------------------------------
